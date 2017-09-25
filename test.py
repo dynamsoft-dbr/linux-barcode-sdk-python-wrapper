de59685ec9f7c13232eabdb614d177c0518e9fb1
yushulx <lingxiao1002@gmail.com>
2017-09-25 16:37:48 +0800

    Remove semicolon

--- test.py
+++ test.py
@@ -6,7 +6,7 @@ def initLicense(license):
 
 def decodeFile(fileName):
     dbr.initLicense("t0068MgAAAGvV3VqfqOzkuVGi7x/PFfZUQoUyJOakuduaSEoI2Pc8+kMwjrojxQgE5aJphmhagRmq/S9lppTkM4w3qCQezxk=")
-    formats = 0x3FF | 0x2000000 | 0x8000000 | 0x4000000; # 1D, QRCODE, PDF417, DataMatrix
+    formats = 0x3FF | 0x2000000 | 0x8000000 | 0x4000000 # 1D, QRCODE, PDF417, DataMatrix
     results = dbr.decodeFile(fileName, formats)
     
     for result in results:
@@ -14,8 +14,8 @@ def decodeFile(fileName):
         print("barcode value: " + result[1])
 
 if __name__ == "__main__":
-    barcode_image = input("Enter the barcode file: ");
+    barcode_image = input("Enter the barcode file: ")
     if not os.path.isfile(barcode_image):
         print("It is not a valid file.")
     else:
-        decodeFile(barcode_image);
+        decodeFile(barcode_image)
